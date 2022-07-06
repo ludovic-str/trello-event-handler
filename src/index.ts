7a85864c16dec4c552858dfda6c4c00ddf00b581
ludovic-str <ludovic.sutter@epitech.eu>
2022-07-06 21:26:57 +0100

    Feat: get activity and add board

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,55 @@
+import {EventEmitter} from "events";
+
 import {Credentials} from "./types/global.types";
+import fetchBoardInfo from "./fetchBoardInfo";
+import {ActionData} from "./types/request/action.types";
 
 export class TrelloEventHandler {
-    #credentials: Credentials;
+    readonly credentials: Credentials;
+    #lastUpdate: number;
+    #e: EventEmitter;
     #boards: string[];
-    #timer: number;
 
     constructor(key: string, token: string) {
-        this.#credentials = {key, token}
+        this.credentials = {key, token}
+        this.#lastUpdate = Date.now();
         this.#boards = [];
-        this.#timer = 0;
+        this.#e = new EventEmitter()
+    }
+
+    start() {
+        setInterval(this.poll, 60 * 1000);
+    }
+
+    stop () {
+        clearInterval();
+    }
+
+    on(event: string, callback: (args: ActionData) => void) {
+        this.#e.on(event, callback);
+    }
+
+    addBoardFromUrl(url: string) {
+        const boardId = url.split("/")[4];
+        this.#boards.push(boardId);
+    }
+
+    addBoardFromId(id: string) {
+        this.#boards.push(id);
+    }
+
+    private poll() {
+        this.#boards.forEach(id => this.getBoardActivity(id));
+    }
+
+    private async getBoardActivity(boardId: string) {
+        const data = await fetchBoardInfo(this.credentials, boardId);
+        if (data === null) return;
+        for (let action of data) {
+            if ((new Date(action.date)).getDate() > this.#lastUpdate) {
+                this.#e.emit(action.type, action);
+            }
+        }
+        this.#lastUpdate = Date.now();
     }
 }
